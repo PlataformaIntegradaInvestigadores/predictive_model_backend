bf0b4c593ee7cfb8bfa6ecd0f60464a9201307d6
paul-lora_wwg <paul.lora@grainger.com>
2025-08-15 03:19:26 -0500

    add centinela url to allowed cors origin

--- app/core/config.py
+++ app/core/config.py
@@ -12,19 +12,14 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Centinela Predictivo de Publicaciones Científicas"
     API_V1_STR: str = "/api/v1"
 
-    # Lista de orígenes permitidos para CORS.
-    # Es crucial agregar la URL de tu frontend de Vercel aquí una vez que la tengas.
+    
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost",
-        "http://localhost:3000",
-        "http://localhost:8080",
+        "https://centinela.epn.edu.ec"
     ]
 
     class Config:
         case_sensitive = True
-        # El archivo .env debe estar en la raíz del directorio 'backend'
         env_file = ".env"
         env_file_encoding = 'utf-8'
 
-# Instancia de la configuración que será importada en otros módulos
 settings = Settings()
